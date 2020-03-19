a0c0283ca37d8633d15ded401df113d0ecb2ac3b
Solène <solene.ulmer-moll@astro.up.pt>
2020-03-19 14:09:11 +0000

    Change the read_data function for the one from pyexoplanetEU with some changes

--- nasa.py
+++ nasa.py
@@ -11,13 +11,15 @@ from urllib import request
 import time
 import math
 import pprint
+import csv
+from collections import OrderedDict
 
 # the link in the "Download Data" button
 download_link = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets'
 
 # to get the directory where NASA Archive data will be stored
 def _create_data_dir():
-    """ Create empty directory where NASA.tsv will be stored """
+    """ Create empty directory where nasa.csv will be stored """
     home = os.path.expanduser("~")
     directory = os.path.join(home, '.nasaarchive')
     if not os.path.exists(directory):
@@ -31,7 +33,7 @@ def _check_data_dir():
 
 
 def get_data_dir():
-    """ Return directory where NASA.tsv is stored """
+    """ Return directory where nasa.csv is stored """
     if not _check_data_dir():
         _create_data_dir()
         
@@ -41,12 +43,12 @@ def get_data_dir():
 #########
 
 def download_data():
-    """ Download NASA Archive data and save it to `NASA.tsv` """
+    """ Download NASA Archive data and save it to `nasa.csv` """
 
     with request.urlopen(download_link) as response:
        data = response.read()
 
-    local_file = os.path.join(get_data_dir(), 'NASA.tsv')
+    local_file = os.path.join(get_data_dir(), 'nasa.csv')
     with open(local_file, 'wb') as f:
         f.write(data)
 
@@ -54,8 +56,8 @@ def download_data():
 
 
 def check_data_age():
-    """ How old is `NASA.tsv`, in days """
-    local_file = os.path.join(get_data_dir(), 'NASA.tsv')
+    """ How old is `nasa.csv`, in days """
+    local_file = os.path.join(get_data_dir(), 'nasa.csv')
     age = time.time() - os.path.getmtime(local_file) # in sec
     return age / (60*60*24) # in days
 
@@ -63,20 +65,22 @@ def check_data_age():
 class DataDict(dict):
     numpy_entries = False
 
-    __doc__ = "SWEET-Cat: a catalog of stellar parameters for stars with planets\n"+\
-              "The catalog and more information can be found at www.astro.up.pt/resources/sweet-cat\n"+\
-              "This dictionary has the same fields as keys "+\
-              "(note that 'sigma' and 'pi' can be used instead of 'σ' and 'π')\n"+\
+    __doc__ = "nasa_archive: a catalog of parameters for known exoplanets.\n" + \
+              "The catalog and more information can be found " \
+              "at https://exoplanetarchive.ipac.caltech.edu/\n" + \
+              "This dictionary has the catalog columns as its keys; " \
+              "see the `.columns()` method.\n" + \
               "Entries are lists, see `to_numpy()` to convert them to numpy arrays."
+
+    def __init__(self, *args, **kwargs):
+        super(DataDict, self).__init__(self, *args, **kwargs)
 
     def __getitem__(self, key):
         # allows to do data[0] to get all columns for the 0th entry in the table
         if isinstance(key, int):
             return {k:v[key] for k, v in self.items()}
 
-        # allows to do data['sigma_feh'] to get data['σ_feh']
-        key = key.replace('sigma', 'σ').replace('pi', 'π')
-        
+
         # allows to do data['key_nonan'] to get data['key'] without NaNs
         if key.endswith('_nonan'):
             val = super().__getitem__(key.replace('_nonan',''))
@@ -99,9 +103,11 @@ class DataDict(dict):
     def __repr__(self):
         return f'NASA Archive data: dictionary with {self.size} entries. '+\
                 'Use .columns() to get the column labels.'
-                
+    def _repr_pretty_(self, p, cycle):
+        return p.text(self.__repr__())
+
     def __len__(self):
-        return len(self.__getitem__('name'))
+        return len(self.__getitem__('pl_hostname'))
 
     def columns(self):
         """ List the available columns """
@@ -109,7 +115,7 @@ class DataDict(dict):
 
     @property
     def size(self):
-        return len(self.__getitem__('name'))
+        return len(self.__getitem__('pl_hostname'))
 
     def to_numpy(self, inplace=True):
         """ 
@@ -126,91 +132,48 @@ class DataDict(dict):
 
 
 def read_data():
-    def val2float(val):
-        if val == '':
-            return math.nan
-        try: 
-            return float(val)
-        except ValueError:
-            return val
-    def val2int(val):
-        try: 
-            return int(val)
-        except ValueError:
-            return val
 
-#    labels = ['name', 'HD', 
-#              'ra', 'dec', 'vmag', 'σ_vmag', 'π', 'σ_π', 'source_π',
-#              'teff', 'σ_teff', 'logg', 'σ_logg', 'LC_logg', 'σ_LC_logg',
-#              'vt', 'σ_vt', 'feh', 'σ_feh', 'mass', 'σ_mass', 'reference',
-#              'homogeneity', 'last_update', 'comments']
-    
-    labels = ['pl_hostname','pl_letter','pl_name','pl_discmethod', 'pl_controvflag',
-              'pl_pnum','pl_orbper','pl_orbpererr1','pl_orbpererr2','pl_orbperlim',
-              'pl_orbpern','pl_orbsmax','pl_orbsmaxerr1','pl_orbsmaxerr2','pl_orbsmaxlim',
-              'pl_orbsmaxn','pl_orbeccen','pl_orbeccenerr1','pl_orbeccenerr2','pl_orbeccenlim',
-              'pl_orbeccenn','pl_orbincl','pl_orbinclerr1','pl_orbinclerr2','pl_orbincllim',
-              'pl_orbincln','pl_bmassj','pl_bmassjerr1','pl_bmassjerr2','pl_bmassjlim',
-              'pl_bmassn','pl_bmassprov','pl_radj','pl_radjerr1','pl_radjerr2',
-              'pl_radjlim','pl_radn','pl_dens','pl_denserr1','pl_denserr2',
-              'pl_denslim','pl_densn','pl_ttvflag','pl_kepflag','pl_k2flag',
-              'ra_str','dec_str','ra','st_raerr','dec',
-              'st_decerr','st_posn','st_dist','st_disterr1','st_disterr2',
-              'st_distlim','st_distn','st_optmag','st_optmagerr','st_optmaglim',
-              'st_optband','gaia_gmag','gaia_gmagerr','gaia_gmaglim','st_teff',
-              'st_tefferr1','st_tefferr2','st_tefflim','st_teffn','st_mass',
-              'st_masserr1','st_masserr2','st_masslim','st_massn','st_rad',
-              'st_raderr1','st_raderr2','st_radlim','st_radn','pl_nnotes',
-              'rowupdate','pl_facility']
+    # Convert the values in integers
+    def apply_int_to_column(data, key):
+        data[key] = [int(v) for v in data[key]]
 
-    # empty dictionary to save data
-    data = {label:[] for label in labels}
+    # Convert values to float and replace empty values by NaN
+    def apply_float_to_column(data, key):
+        data[key] = [float(v) if v!='' else math.nan for v in data[key]]
 
     # read the file
-    local_file = os.path.join(get_data_dir(), 'NASA.tsv')
-    lines = open(local_file).readlines()
+    local_file = os.path.join(get_data_dir(), 'nasa.csv')
+    with open(local_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        lines = [row for row in reader]
+
+    # lines is a list of (ordered) dicts; transform it to a (ordered) dict of lists
+    data = OrderedDict({k: [dic[k] for dic in lines] for k in lines[0]})
+
+    # column labels were read automatically by the csv.DictReader
+    labels = list(data.keys())
 
     nlab, nlin = len(labels), len(lines)
-    print(f'There are {nlab} columns with {nlin} entries each in `NASA.tsv`')
-
-    for line in lines:
-        # split the columns
-        vals = line.strip().split(',')
-        # make some columns into ints
-        vals = vals[:4] + list(map(val2int, vals[4:6])) + list(map(val2int, vals[9:11])) \
-        + list(map(val2int, vals[14:16])) + list(map(val2int, vals[19:21])) \
-        + list(map(val2int, vals[24:26])) + list(map(val2int, vals[29:32])) \
-        + list(map(val2int, vals[35:37])) + list(map(val2int, vals[40:47])) \
-        + list(map(val2int, vals[51])) + list(map(val2int, vals[55:57])) \
-        + list(map(val2int, vals[59:61])) + list(map(val2int, vals[63])) \
-        + list(map(val2int, vals[67:69])) + list(map(val2int, vals[72:74])) \
-        + list(map(val2int, vals[77:]))
-        
-        # make some columns into floats
-        vals[6:9] = list(map(val2float, vals[6:9]))
-        vals[11:14] = list(map(val2float, vals[11:14]))
-        vals[16:19] = list(map(val2float, vals[16:19]))
-        vals[21:24] = list(map(val2float, vals[21:24]))
-        vals[26:29] = list(map(val2float, vals[26:29]))
-        vals[32:35] = list(map(val2float, vals[32:35]))
-        vals[37:40] = list(map(val2float, vals[37:40]))
-        vals[47:51] = list(map(val2float, vals[47:51]))
-        vals[52:55] = list(map(val2float, vals[52:55]))
-        vals[57:59] = list(map(val2float, vals[57:59]))
-        vals[61:63] = list(map(val2float, vals[61:63]))
-        vals[64:67] = list(map(val2float, vals[64:67]))
-        vals[69:72] = list(map(val2float, vals[69:72]))
-        vals[74:77] = list(map(val2float, vals[74:77]))
-
-        for i, v in enumerate(data.values()):
-            v.append(vals[i])
+    print(f'There are {nlab} columns with {nlin} entries each in `nasa.csv`')
 
     data = DataDict(**data)
+
+    # transform columns to integers or floats or keep the strings
+    for col in data.keys():
+        try:
+            apply_int_to_column(data, col)
+        except ValueError:
+            try:
+                apply_float_to_column(data, col)
+            except ValueError:
+                pass
+
     return data
 
 
+
 def get_data():
-    local_file = os.path.join(get_data_dir(), 'NASA.tsv')
+    local_file = os.path.join(get_data_dir(), 'nasa.csv')
 
     if not os.path.exists(local_file):
         print ('Downloading NASA data')
@@ -218,10 +181,10 @@ def get_data():
     
     age = check_data_age()
     if age > 5:
-        print ('Data in `NASA.tsv` is older than 5 days, downloading.')
+        print ('Data in `nasa.csv` is older than 5 days, downloading.')
         download_data()
     else:
-        print ('Data in `NASA.tsv` is recent.')
+        print ('Data in `nasa.csv` is recent.')
 
     data = read_data()
     return data
